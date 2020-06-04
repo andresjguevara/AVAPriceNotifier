c08ffd7531427956d9ec87ac78285f34d5a5c071
andresjguevara <guevara.a@husky.neu.edu>
2020-06-04 10:03:57 -0400

    Added support to send email to recipients

--- AVAPriceNotifier.py
+++ AVAPriceNotifier.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 
 import requests
+import smtplib
+import ssl
+import datetime
 from html.parser import HTMLParser
 
 '''
-1- Connect to host
-2- Send get request
-3 - Parse HTML
-4- Check for min price
-5- Send email
-6- Repeat every few hours
+1- Get data from website
+2- Send email
+3- Repeat every few hours
 '''
 
 class AVAParser(HTMLParser):
@@ -26,7 +26,6 @@ class AVAParser(HTMLParser):
             for name, value in attrs:
                 if name == "class" and value == "sidebar-apartment-prices":
                     self.recording = True
-                    # print("Apartment Data")
 
     def handle_endtag(self, tag):
         if tag == "ul" and self.recording:
@@ -37,12 +36,41 @@ class AVAParser(HTMLParser):
             self.data.append(data)
 
 
+def create_message(data):
+    current_time = datetime.datetime.now()
+    day = current_time.day
+    month = current_time.month
+    hours = current_time.hour
+    minutes = current_time.minute
+    message = "Subject: AVA North Station Prices\n" \
+              "Hi, \n\n" \
+              "The prices for AVA North Station on {0}/{1} at {2}:{3} are:\n" \
+              "".format(month, day, hours, minutes)
+    for price in data:
+        message += price + "\n"
+    return message
+
+
+def send_email(message, receivers):
+    port = 465
+    password = "DevelopmentNEU20"
+    context = ssl.create_default_context()
+    sender = "ajgc.development@gmail.com"
+
+    with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
+        server.login("ajgc.development@gmail.com", password)
+        for to in receivers:
+            server.sendmail(sender, to, message)
+    return
+
 
 def run():
     res = requests.get("https://www.avaloncommunities.com/massachusetts/boston-apartments/avalon-north-station")
     parser = AVAParser()
     parser.feed(res.text)
-    print(parser.data)
+    message = create_message(parser.data)
+    to = ["ajgc98@gmail.com", "marcef1996@gmail.com"]
+    send_email(message, to)
 
 
 if __name__ == '__main__':
